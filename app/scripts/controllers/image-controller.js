12a69c7371954e801a7de94722b1bcc8ca3be842
Konrad Kleine <konrad.wilhelm.kleine@gmail.com>
2014-10-22 09:29:50 +0200

    Fix #3 Show total size of all layers for an image

--- app/scripts/controllers/image-controller.js
+++ app/scripts/controllers/image-controller.js
@@ -13,4 +13,19 @@ angular.module('image-controller', ['registry-services'])
     $scope.imageId = $route.current.params['imageId'];
     $scope.imageDetails = Image.query( {imageId: $scope.imageId} );
     $scope.imageAncestry = Ancestry.query( {imageId: $scope.imageId} );
+    /**
+     * Calculates the total download size for the image based on
+     * it's ancestry.
+     */
+    $scope.totalImageSize = null;
+    $scope.calculateTotalImageSize = function() {
+      $scope.totalImageSize = 0;
+      angular.forEach($scope.imageAncestry, function (id, key) {
+        Image.get( {imageId: id} ).$promise.then(function (result) {
+          if (!isNaN(result.Size-0)) {    
+            $scope.totalImageSize += result.Size;
+          }
+        });
+      });
+    };
   }]);
