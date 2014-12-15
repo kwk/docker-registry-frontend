78189db245fa4484fbbcce73fd56e2f01606d5d7
rinrinne <rinrin.ne@gmail.com>
2014-12-15 20:38:52 +0900

    Rename "ENV_BROWSE_MODE" to "ENV_MODE_BROWSE_ONLY"

--- start-apache.sh
+++ start-apache.sh
@@ -24,8 +24,8 @@ fi
 echo "{\"host\": \"$ENV_DOCKER_REGISTRY_HOST\", \"port\": $ENV_DOCKER_REGISTRY_PORT}" > /var/www/html/registry-host.json
 
 # information about browse mode.
-[[ -z "$ENV_BROWSE_MODE" ]] && ENV_BROWSE_MODE=false
-echo "{\"browse\":\"$ENV_BROWSE_MODE\"}" > /var/www/html/app-mode.json
+[[ -z "$ENV_MODE_BROWSE_ONLY" ]] && ENV_MODE_BROWSE_ONLY=false
+echo "{\"browse\":\"$ENV_MODE_BROWSE_ONLY\"}" > /var/www/html/app-mode.json
 
 # Optionally enable Kerberos authentication and do some parameter checks
 if [ -n "$ENV_AUTH_USE_KERBEROS" ]; then
