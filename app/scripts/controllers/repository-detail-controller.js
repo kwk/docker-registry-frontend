b4877284d149c33391ec47683bcfed85ed59bf05
rinrinne <rinrin.ne@gmail.com>
2014-12-15 20:13:12 +0900

    Adjust documentation and remove unnecessary code

--- app/scripts/controllers/repository-detail-controller.js
+++ app/scripts/controllers/repository-detail-controller.js
@@ -4,21 +4,11 @@
  * @ngdoc function
  * @name docker-registry-frontend.controller:RepositoryDetailController
  * @description
- * # RepositoryController
+ * # RepositoryDetailController
  * Controller of the docker-registry-frontend
  */
 angular.module('repository-detail-controller', ['app-mode-services'])
   .controller('RepositoryDetailController', ['$scope', '$route', '$routeParams', '$location', 'AppMode',
   function($scope, $route, $routeParams, $location, AppMode){
-
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
-
-    $scope.searchTerm = $route.current.params['searchTerm'];
-    $scope.repositoryUser = $route.current.params['repositoryUser'];
-    $scope.repositoryName = $route.current.params['repositoryName'];
-    $scope.repository = $scope.repositoryUser + '/' + $scope.repositoryName;
-
     $scope.appMode = AppMode.query();
   }]);
