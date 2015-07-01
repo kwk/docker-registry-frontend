d24610dcc37a5eac78435929155737c37966ba17
Martin Bydzovsky <martin.bydzovsky@socialbakers.com>
2015-07-01 14:09:08 +0200

    another fix of repositories not alwawys deleted

--- app/scripts/controllers/delete-repository-controller.js
+++ app/scripts/controllers/delete-repository-controller.js
@@ -8,44 +8,51 @@
  * Controller of the docker-registry-frontend
  */
 angular.module('delete-repository-controller', ['registry-services'])
-  .controller('DeleteRepositoryController', ['$scope', '$route', '$modalInstance', '$window', 'Repository', 'items', 'information',
-  function($scope, $route, $modalInstance, $window, Repository, items, information){
+  .controller('DeleteRepositoryController', ['$scope', '$route', '$q', '$modalInstance', '$window', 'Repository', 'items', 'information',
+  function($scope, $route, $q, $modalInstance, $window, Repository, items, information){
     $scope.items = items;
     $scope.information = information;
-    
+
     // Callback that triggers deletion of tags and reloading of page
     $scope.ok = function () {
+      var requests = [];
+
       angular.forEach($scope.items, function(value, key) {
         var repoStr = value;
         var repoUser = value.split("/")[0];
         var repoName = value.split("/")[1];
-      
+
         var repo = {
           repoUser: repoUser,
           repoName: repoName
         };
-        
-        Repository.delete(repo,
-          // success
-          function(value, responseHeaders) {
-            toastr.success('Deleted repository: ' + repoStr);
-          },
-          // error
-          function(httpResponse) {
-            toastr.error('Failed to delete repository: ' + repoStr + ' Response: ' + httpResponse.statusText);
-          }
-        );
+
+        var deferred = $q.defer();
+        requests.push(deferred.promise);
+
+        var success = function() {
+          toastr.success('Deleted repository: ' + repoStr);
+          deferred.resolve();
+        }
+
+        var fail = function(httpResponse) {
+          toastr.error('Failed to delete repository: ' + repoStr + ' Response: ' + httpResponse.statusText);
+          deferred.reject();
+        }
+
+        Repository.delete(repo, success, fail);
+
       });
-      
-      $modalInstance.close();
-      
-      // Go to the repositories page
-      $window.location.href = '#/repositories';
-      $route.reload();
+      $q.all(requests).then(function(){
+        $modalInstance.close();
+        $window.location.href = '#/repositories';
+        $route.reload();
+      });
+
     };
 
     $scope.cancel = function () {
       $modalInstance.dismiss('cancel');
     };
-    
+
   }]);
