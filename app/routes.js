fced969fb783e52bb0a7cdb80fbbdbfc9e8a225b
Konrad Kleine <konrad.wilhelm.kleine@gmail.com>
2016-01-26 16:43:06 +0100

    Moved files around a bit for a more standatd MEAN-like dir structure

--- app/routes.js
+++ app/routes.js
@@ -58,6 +58,6 @@ module.exports = function (app) {
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendfile('./public/views/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
