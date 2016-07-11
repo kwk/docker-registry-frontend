19c47625fe7c9648670a8e611f626fd0035aaefe
Marc Abramowitz <marc@marc-abramowitz.com>
2016-07-11 07:07:04 -0700

    test/spec/controllers/main.js: Check appVersion & registryHost

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -7,13 +7,24 @@ describe('MainController', function() {
 
   var ctrl;
   var scope = {};
+  var $httpBackend;
 
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($controller, _$httpBackend_) {
     ctrl = $controller('MainController', {$scope: scope});
+    $httpBackend = _$httpBackend_;
   }));
 
   it('should attach an appVersion and registryHost to the scope', function() {
-    var scope_keys = Object.keys(scope).sort();
-    expect(scope_keys).toEqual(['appVersion', 'registryHost']);
+    var scopeKeys = Object.keys(scope).sort();
+    expect(scopeKeys).toEqual(['appVersion', 'registryHost']);
+
+    var expectedAppVersion = {"git": {"sha1": "foo", "ref": "bar"}};
+    var expectedRegistryHost = {"host": "path-to-your-registry", "port": 80};
+    $httpBackend.expectGET('/app-version.json').respond(expectedAppVersion);
+    $httpBackend.expectGET('/registry-host.json').respond(expectedRegistryHost);
+    $httpBackend.flush();
+    jasmine.addCustomEqualityTester(angular.equals);
+    expect(scope.appVersion).toEqual(expectedAppVersion);
+    expect(scope.registryHost).toEqual(expectedRegistryHost);
   });
 });
